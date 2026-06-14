5a7115a19af1bf3e5638db5e4e2017825653cee0
vikalpthatsall <vkumar.be19@gmail.com>
2026-06-14 13:11:47 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_diff=0
+        for i in range(len(prices)):
+            for j in range(i,len(prices)):
+                max_diff = max(max_diff, prices[j]-prices[i])
+
+        return max_diff
+    
